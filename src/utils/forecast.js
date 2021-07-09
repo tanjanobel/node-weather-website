b581127c676d017e2c3babc0c5f15368ede95947
Tanja Nobel <tanja@nobel.swiss>
2021-07-09 14:51:02 +0200

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degres out. It feels like ' +
+                body.current.feelslike + ' degres out. The humidity is ' + body.current.humidity + '%.'
+            )
         }
     })
 };
